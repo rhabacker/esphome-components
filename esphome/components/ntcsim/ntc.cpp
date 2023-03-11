bd45ca0152eedca90658df6a5fd765a5c1643165
Ralf Habacker <ralf.habacker@freenet.de>
2023-03-11 15:40:32 +0100

    Add support for the reverse operation to the ntcsim component
    With this change the ntcsim component can calculate the resistance
    from a temperature required to make NTC's "smart".
    
    This component get's a new 'output' property to which the normalized
    resistance value (0..1.0) will be send to and the property
    'output_resistance' to specify the nominal resistance value of the
    simulated NTC. The coefficients for the NTC are specified by the
    'calibration' property.

--- esphome/components/ntcsim/ntc.cpp
+++ esphome/components/ntcsim/ntc.cpp
@@ -2,30 +2,46 @@
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace ntc {
+namespace ntcsim {
 
-static const char *const TAG = "ntc";
+static const char *const TAG = "ntcsimulation";
 
-void NTC::setup() {
-  this->sensor_->add_on_state_callback([this](float value) { this->process_(value); });
-  if (this->sensor_->has_state())
-    this->process_(this->sensor_->state);
+void NTCSimulation::setup() {
+  sensor_->add_on_state_callback([this](float value) { process_(value); });
+  if (sensor_->has_state())
+    process_(sensor_->state);
+  dump_config();
 }
-void NTC::dump_config() { LOG_SENSOR("", "NTC Sensor", this) }
-float NTC::get_setup_priority() const { return setup_priority::DATA; }
-void NTC::process_(float value) {
+void NTCSimulation::dump_config() {
+  LOG_SENSOR("", "NTC Simulation", this);
+  ESP_LOGI(TAG, "  Coefficient a: %.8f", a_);
+  ESP_LOGI(TAG, "  Coefficient b: %.8f", b_);
+  ESP_LOGI(TAG, "  Coefficient c: %.8f", c_);
+}
+
+float NTCSimulation::get_setup_priority() const { return setup_priority::DATA; }
+
+void NTCSimulation::process_(float value) {
   if (std::isnan(value)) {
-    this->publish_state(NAN);
+    publish_state(NAN);
     return;
   }
-
-  double lr = log(double(value));
-  double v = this->a_ + this->b_ * lr + this->c_ * lr * lr * lr;
-  auto temp = float(1.0 / v - 273.15);
-
-  ESP_LOGD(TAG, "'%s' - Temperature: %.1f°C", this->name_.c_str(), temp);
-  this->publish_state(temp);
+  double K = 273.15;
+  double t = value;
+  // https://de.wikipedia.org/wiki/Steinhart-Hart-Gleichung#cite_note-stein2_s4-3
+  double T = value + K;
+  double y = (a_ - 1 / (T)) / (2 * c_);
+  double x = sqrt(pow(b_ / (3 * c_), 3) + y * y);
+  double resistance = exp(pow(x - y, 1 / 3.0) - pow(x + y, 1 / 3.0));
+  ESP_LOGI(TAG, "got resistor value %f for temperature %f", resistance, t);
+  ESP_LOGD(TAG, "'%s' - Resistance: %.1f Ohm", this->name_.c_str(), resistance);
+  publish_state(resistance);
+  if (output_) {
+    double normalized_resistance = resistance / resistance_;
+    ESP_LOGI(TAG, "send normalized resistor value %f to output device", normalized_resistance);
+    output_->set_level(normalized_resistance);
+  }
 }
 
-}  // namespace ntc
+}  // namespace ntcsim
 }  // namespace esphome
